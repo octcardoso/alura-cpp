7c218945cc1c0a958ab8cf48869c6439620c847a
octcardoso <octavio.au.cardoso@gmail.com>
2024-09-14 03:06:31 -0300

    inicio jogo da forca alura

--- jogo-da-forca.cpp
+++ jogo-da-forca.cpp
@@ -1,113 +1,37 @@
 #include <iostream>
 #include <string>
-#include <map>
 
 using namespace std;
-string message = "Seja bem vindo ao jogo da forca.";
-string secretWord = "MELANCIA";
-map<char, bool> attempedLetters;
-char guess;
 
-void initMessage(string welcomeMessage) {
-	
-	string header;
-	welcomeMessage = "* " + welcomeMessage + " *";
-	for(char letter : welcomeMessage) {
-		header += "*";
-	}
-	
-	cout << header << endl;
-	cout << welcomeMessage << endl;
-	cout << header << endl;
+const string PALAVRA_SECRETA = "MELANCIA";
 
-}
-
-bool guessedLetter(char guess) {
+bool letra_existe(char chute) {
 	
-	for(char letter : secretWord) {
-		
-		if(letter == guess) {
-	
-			attempedLetters[guess] = true;
+	for(char letra : PALAVRA_SECRETA) {
+		if(chute == letra) {
 			return true;
-	
 		}
-	
 	}
 	
-	attempedLetters[guess] = false;
-	return false;	
-
-}
-
-void printWord() {
-	
-	for(char letter : secretWord) {
-		
-		if(attempedLetters[letter]) {
-
-			cout << letter << " ";
-
-		} else {
-
-			cout << "_ ";
-
-		}
-		
-	}
-
+	return false;
 }
 
 int main() {
 	
-	bool alive = true;
-	
-	initMessage(message);
-	cout << endl << secretWord << endl;
-	printWord();
-	cout << endl;
-	cout << "A palavra tem " << secretWord.length() << " letras." << endl;
-	cout << "Boa sorte." << endl;
+	bool acertou, enforcou = false;
+	char chute;
 
-	while(alive) {
-		
-		cout << "Insira o seu chute: ";
-		cin >> guess;
-		cout << endl;
-
-		if(guessedLetter(guess)) {
-			cout << "Você acertou, a letra " << guess << " está dentro da palavra" << endl;
-			printWord();
+	while(!acertou && !enforcou) {
+		cout << "Digite o seu chute: ";
+		cin >> chute;
+		if(letra_existe(chute)) {
+			cout << "Você acertou, a letra " << chute << " está na palavra" << endl;
 			cout << endl;
 		} else {
-			cout << "Você errou, a letra " << guess << " não está dentro da palavra" << endl;
-			printWord();
+			cout << "Você errou, a letra " << chute << " não está na palavra" << endl;
 			cout << endl;
 		}
-		
-
-		
 	}
-
+	
 	return 0;
-
 }
-
-
-
-/*
-	 * 
-	 *
-	 * bem vindo ao jogo da forca
-	 * aperte 0 para iniciar e 2 para sair
-	 * 0. iniciar
-	 * 1. sair
-	 *
-	 * randomiza uma palavra
-	 * imprime _ da palavra
-	 * começa os chutes
-	 * para cada chute errado vai perdendo vida...
-	 *
-	 * */	
-
-
