a45afc48266fd0a3a4a8b34f457804fac9e476d8
octcardoso <octavio.au.cardoso@gmail.com>
2024-09-14 02:49:43 -0300

    jogo base finalizado

--- jogo-da-forca.cpp
+++ jogo-da-forca.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 string message = "Seja bem vindo ao jogo da forca.";
+string secretWord = "MELANCIA";
+map<char, bool> attempedLetters;
+char guess;
 
 void initMessage(string welcomeMessage) {
 	
@@ -19,9 +22,72 @@ void initMessage(string welcomeMessage) {
 
 }
 
+bool guessedLetter(char guess) {
+	
+	for(char letter : secretWord) {
+		
+		if(letter == guess) {
+	
+			attempedLetters[guess] = true;
+			return true;
+	
+		}
+	
+	}
+	
+	attempedLetters[guess] = false;
+	return false;	
+
+}
+
+void printWord() {
+	
+	for(char letter : secretWord) {
+		
+		if(attempedLetters[letter]) {
+
+			cout << letter << " ";
+
+		} else {
+
+			cout << "_ ";
+
+		}
+		
+	}
+
+}
+
 int main() {
 	
+	bool alive = true;
+	
 	initMessage(message);
+	cout << endl << secretWord << endl;
+	printWord();
+	cout << endl;
+	cout << "A palavra tem " << secretWord.length() << " letras." << endl;
+	cout << "Boa sorte." << endl;
+
+	while(alive) {
+		
+		cout << "Insira o seu chute: ";
+		cin >> guess;
+		cout << endl;
+
+		if(guessedLetter(guess)) {
+			cout << "Você acertou, a letra " << guess << " está dentro da palavra" << endl;
+			printWord();
+			cout << endl;
+		} else {
+			cout << "Você errou, a letra " << guess << " não está dentro da palavra" << endl;
+			printWord();
+			cout << endl;
+		}
+		
+
+		
+	}
 
 	return 0;
 
